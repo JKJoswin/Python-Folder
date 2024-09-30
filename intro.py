a79b51cf9d1d81ca70c992a00129474f27380fc9
Joswin <joswinjayakumar@gmail.com>
2024-09-30 17:52:01 +0530

    Python 1!!!

---
+++ intro.py
@@ -0,0 +1,6 @@
+name="Joswin";
+print("Hi! I am ", name,".")
+birthyear=2013;
+presentyear=2024;
+presentage=presentyear-birthyear;
+print("I am ",presentage," years old.")
---
+++ p1st.py
@@ -0,0 +1,1 @@
+print("Hello! I am Joswin! This is my first python project!")
